e1335e133d4b68aca4d442a105baf298bf6dec8d
Robin Rosh <63775-RobinRosh@users.noreply.drupalcode.org>
2024-01-25 18:19:43 +0530

    Props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,41 @@ import Content from './Content'
 import Footer from './Footer'
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
+import { useState } from "react";
 
 function App() {
+
+  const [checkItems, setCheckItems] = useState(
+    [
+      {
+        id: 1,
+        checked: true,
+        label: "Item - "
+      },
+      {
+        id: 2,
+        checked: true,
+        label: "Item - "
+      },
+      {
+        id: 3,
+        checked: true,
+        label: "Item - "
+      }
+    ]
+  );
+  const updateItems = (id) => {
+    const updateCheckItems = checkItems.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
+    setCheckItems(updateCheckItems);
+  }
+  const removeItem = (id) => {
+    const updateCheckItems = checkItems.filter((item) => item.id !== id)
+    setCheckItems(updateCheckItems);
+  }
   return (
     <div >
       {/* <Header /> */}
-      <Content />
+      <Content checkItems = {checkItems} updateItems = {updateItems} removeItem = {removeItem} />
       {/* <Footer /> */}
     </div>
   );
--- src/Content.js
+++ src/Content.js
@@ -1,46 +1,16 @@
 import React from "react";
-import { useState } from "react";
 import { TfiTrash } from "react-icons/tfi";
 import ListGroup from 'react-bootstrap/ListGroup';
-import Button from 'react-bootstrap/Button';
 
-
-
-const Content = () => {
-    const [checkItems, setcheckItems] = useState(
-        [
-            {
-                id: 1,
-                checked: true,
-                label: "List one"
-            },
-            {
-                id: 2,
-                checked: true,
-                label: "List two"
-            },
-            {
-                id: 3,
-                checked: true,
-                label: "List three"
-            }
-        ]
-    ); 
-    const changeCheck = (id) => {
-        const updateCheckItems = checkItems.map((item) => item.id === id ? {...item,checked:!item.checked} : item)
-        setcheckItems(updateCheckItems);
-    }
-    const removeItem = (id) => {
-        const updateCheckItems = checkItems.filter((item) => item.id !== id )
-        setcheckItems(updateCheckItems);
-    }
+const Content = ({checkItems, updateItems, removeItem}) => {
+    console.log(checkItems.map((item) => item))
     return (
         <div>
             <ListGroup as="ul">
-                {checkItems.map((item) => (
-                    <ListGroup.Item as="li" key={item.id}>
-                        <input type="checkbox" checked={item.checked} onChange={()=>changeCheck(item.id)}/>
-                        <label onClick={()=>changeCheck(item.id)}>{item.label}</label>
+                {checkItems.map((item, index) => (
+                    <ListGroup.Item as="li">
+                        <input type="checkbox" checked={item.checked} onChange={()=>updateItems(item.id)}/>
+                        <label onClick={()=>updateItems(item.id)}>{item.label}{index+1}</label>
                         <TfiTrash role="button" cursor="pointer" onClick={() => removeItem(item.id)} />
                     </ListGroup.Item>
                 ) )}
